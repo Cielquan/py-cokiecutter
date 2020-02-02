7fe7bb291b654b44698cc58aebcdaec7d7f12008
Cielquan <cielquan@protonmail.com>
2020-02-02 15:28:40 +0100

    fixed build configs

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -64,8 +64,5 @@ import setuptools
 
 
 setuptools.setup(
-    use_scm_version={
-        "write_to": "src/{{cookiecutter.project_slug}}/version.py",
-        "write_to_template": "__version__ = {version!r}",
-    },
+    setup_requires=["setuptools_scm"], use_scm_version=True,
 )
