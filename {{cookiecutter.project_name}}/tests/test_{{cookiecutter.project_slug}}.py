d1455e476c4a349a218496b7d406bd95537c4029
Christian Riedel <cielquan@protonmail.com>
2021-01-21 12:44:07 +0100

    fix problem with empty test

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -8,3 +8,13 @@
     :copyright: (c) {{cookiecutter.year}}, {{cookiecutter.full_name}} and AUTHORS
     :license: {{cookiecutter.license}}, see LICENSE for details
 """  # noqa: D205,D208,D400
+from {{cookiecutter.project_slug}} import {{cookiecutter.project_slug}}
+
+
+def test_dummy() -> None:
+    """Test a dummy to silence pytest exit code 5."""
+    print({{cookiecutter.project_slug}})
+
+    result = 1 + 1
+
+    assert result == 2
